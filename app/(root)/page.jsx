758467a43812c13ffbf173af6d5261bba6bb3108
Ashish Thomas <ashishthomas2202@gmail.com>
2023-10-11 12:33:00 -0400

    added user authentication

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <main>Home Admin Dashboard</main>;
+// import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+export default function SetupPage() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <h1>This is a protected page</h1>
+    </div>
+  );
 }
